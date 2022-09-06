0c1806e546d82a303cf285ba2dc5eb82ae695575
Martin Amato <106878671+MartinAmato12@users.noreply.github.com>
2022-09-06 19:36:03 -0300

    Add files via upload

--- sintaxis_y_variables/main.js
+++ sintaxis_y_variables/main.js
@@ -1,10 +1,48 @@
-let clientes = Number(prompt("Ingrese el numero de clientes:"));
-const nombre = [];
+let producto ="";
+let precio = 0;
+let seguirComprando = false;
+let cantidad = 0;
+let precioTotal = 0;
 
-for(let i = 1; i <= clientes; i++){
-    let cliente = prompt("ingresar nombre del cliente")
-    alert('el cliente n.'+i+' es '+ cliente)
-    nombre.push(cliente);
+function calcularPrecio(producto,precio){
+    return precioTotal += producto * precio;
 }
+do{
+    producto = prompt("Hola queres comprar Paquete, Album, Album tapa dura, Album y paquetes o todo?");
+    cantidad = parseInt(prompt("Cuántos queres comprar?"));
 
-console.log(nombre);
+    switch(producto){
+        case "Paquete" :
+            precio = 150;
+            break;
+
+        case "Album" :
+            precio = 750;
+            break;
+        
+        case "Album tapa dura":
+            precio = 3000;
+            break;
+
+        case "Album y paquetes":
+            precio = 900;
+            break;
+
+        default :
+            alert("Ningun producto fue seleccionado, por favor intente nuevamente");
+            precio = 0;
+            cantidad = 0;
+    }
+
+    calcularPrecio(precio,cantidad);
+
+    seguirComprando = confirm("¿Querés agregar mas productos?");
+        
+}while(seguirComprando);
+
+alert("El precio total es de: " + precioTotal);
+
+let FinCompra = confirm("¿Desea Finalizar Su Compra?");
+
+if(FinCompra) alert("Su compra fue realizada con exito");
+if(!FinCompra) alert("Vuelva pronto");
